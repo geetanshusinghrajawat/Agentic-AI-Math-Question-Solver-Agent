c13a403308b38ccaa68ddbdead49269efca810bb
Geetanshu Singh Rajawat <geetanshusingh01@gmail.com>
2026-08-20 22:20:55 +0530

    Refactor AI_Math_Solver for improved functionality

--- AI_Math_Solver.py
+++ AI_Math_Solver.py
@@ -17,7 +17,7 @@ if not groq_api:
     st.info('Please Provide Groq API key')
     st.stop()
 
-model=ChatGroq(model='qwen/qwen3.6-27b',groq_api_key=groq_api)
+model=ChatGroq(model='qwen/qwen3.6-27b',groq_api_key=groq_api,reasoning_format="hidden")
 
 ######----------- INITIALIZING AGENTS -----------###############
 
